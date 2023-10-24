5df64f0d3ff02629f5a8252ce91c95f412f26a24
Miguel Román <miguel.rv375@gmail.com>
2023-10-24 14:46:53 -0500

    actualizar ruta de la imagen

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
    <h1>welcome</h1>
    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quia laborum eos, adipisci ducimus alias magni minima nulla quis laudantium facilis atque, perferendis quaerat pariatur recusandae omnis laboriosam? Rerum, dolorum!</p>
    <div>
-      <img src="descarga.png" alt="descargar">
+      <img src="images/descarga.png" alt="descargar">
    </div>
 </body>
 </html>
