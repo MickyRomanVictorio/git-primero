31c87f12ec81d15156a3eb5ab7f8efd454f30108
Miguel Román <miguel.rv375@gmail.com>
2023-10-24 09:28:16 -0500

    agregar imagen y modifica index

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php 
-
-   echo "pruebas con git";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>sdfsdfsdfsfs</title>
+</head>
+<body>
+   <h1>welcome</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quia laborum eos, adipisci ducimus alias magni minima nulla quis laudantium facilis atque, perferendis quaerat pariatur recusandae omnis laboriosam? Rerum, dolorum!</p>
+   <div>
+      <img src="descarga.png" alt="descargar">
+   </div>
+</body>
+</html>
